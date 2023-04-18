dc34d830b128b0c45d41c2e5b43e445697bb1aec
Brian Porter <brianpmusic@gmail.com>
2023-04-18 15:41:57 -0700

    draft front in using HTML

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -24,24 +24,30 @@ def index():
 
 @app.route('/loan/estimate', methods=['POST'])
 def get_loan_estimate():
-    # credit_score = request.json.get('credit_score')
-    # annual_income = request.json.get('annual_income')
-    # max_monthly_payment = request.json.get('max_monthly_payment')
+    credit_score = request.json.get('credit_score')
+    annual_income = request.json.get('annual_income')
+    max_monthly_payment = request.json.get('max_monthly_payment')
     # Calculate the loan estimate based on the inputs
     # ...
     pass
 
 @app.route('/loan/application', methods=['POST'])
 def apply_for_loan():
-    # user_id = request.json.get('user_id')
-    # physician_id = request.json.get('physician_id')
-    # procedure_date = request.json.get('procedure_date')
-    # down_payment = request.json.get('down_payment')
-    # monthly_payment = request.json.get('monthly_payment')
-    # apr = request.json.get('apr')
-    # Create a new loan application in the database
-    # ...
-    pass
+    if request.method == 'POST':
+        user_id = request.form.get('user_id')
+        physician_id = request.form.get('physician_id')
+        procedure_date = request.form.get('procedure_date')
+        down_payment = request.form.get('down_payment')
+        monthly_payment = request.form.get('monthly_payment')
+        apr = request.form.get('apr')
+        # Create a new loan application in the database
+        # ...
+        return "Loan application submitted!"
+    else:
+        # Load the user and physician data for display in the HTML form
+        users = User.query.all()
+        physicians = Physician.query.all()
+        return render_template('loan_application.html', users=users, physicians=physicians)
 
 @app.route('/loan/application/<int:loan_id>', methods=['GET'])
 def get_loan_application_status(loan_id):
@@ -60,6 +66,16 @@ def get_loan_details(loan_id):
     # Get the loan details from the database
     # ...
     pass
+
+# renders the get_loan_estimate.html template when the user visits the /gui/loan/estimate URL
+@app.route('/gui/loan/estimate')
+def get_loan_estimate_gui():
+    return render_template('get_loan_estimate.html')
+
+# renders the loan_application.html template when the user visits the /gui/loan/apply URL
+@app.route('/gui/loan/apply')
+def apply_for_loan_gui():
+    return render_template('loan_application.html')
 
 if __name__ == '__main__':
     app.run()
